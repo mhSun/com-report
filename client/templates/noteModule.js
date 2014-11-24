51f4349920dc4f456233cd698f64f722706debb1
menghua.sun <mhsun1992@gmail.com>
2014-11-24 19:06:05 +0800

    change save event

--- client/templates/noteModule.js
+++ client/templates/noteModule.js
@@ -1,6 +1,8 @@
 
 if (Meteor.isClient) {
 
+	/* noteModule Template */
+
 	Template.noteModule.helpers({
 		times: function () {
 			return Times.find();
@@ -15,20 +17,42 @@ if (Meteor.isClient) {
 
 	Template.noteModule.events({
 		"click .js-note-new": function () {
-			Notes.insert(
-				{
-					title: '新建事件',
-					content: '',
-					progress: '',
-					risk: '',
-					timeId: Session.get("currentTime"),
-					userId: Meteor.userId(),
-					createAt: new Date(),
-					updateAt: new Date()
-				}
-			);
+			/* 添加新事件 */
+		},
+		"click .js-note-save": function () {
+			/* 保存事件的更改 */
+
+			// 保存事件
+
+			// 触发取消点击事件
+			$(".js-note-cancel").click();
+		},
+		"click .js-note-cancel": function () {
+			/* 取消事件的更改 */
+			// 显示编辑按钮
+			$(".js-note-item.absolute").find(".js-note-edit").removeClass("hide");
+
+			// 转为列表显示
+			$(".js-note-item").removeClass("absolute").removeClass("hide");
+
+			// 改变菜单栏按钮
+			$(".js-note-new").removeClass("hide");
+			$(".js-note-save, .js-note-cancel, .js-note-delete").addClass("hide");
+
+			// 取消编辑状态
+			$(".js-edit-title, .js-edit-content, .js-edit-progress, .js-edit-risk").attr("contenteditable", "false");
+		},
+		"click .js-note-delete": function () {
+			/* 删除事件 */
+
+			// 删除事件
+
+			// 触发取消点击事件
+			$(".js-note-cancel").click();
 		}
 	});
+
+	/* time Template */
 
 	Template.time.helpers({
 		current: function () {
@@ -44,38 +68,35 @@ if (Meteor.isClient) {
 		}
 	});
 
+	/* note Template */
+
 	Template.note.helpers({
 		editable: function () {
-			return Meteor.userId() === this.userId;
+			return Meteor.userId() === this.userId ? '' : 'hide';
 	    }
 	});
 
 	Template.note.events({
-		'blur': function (events) {
-			var key = events.target.classList[1];
-			var value = events.target.innerHTML;
-			
-			if (key === "title") {
-				Notes.update(this._id,
-					{$set: {title: value, updateAt: new Date()}}
-				);
-			} else if (key === "content") {
-				Notes.update(this._id,
-					{$set: {content: value, updateAt: new Date()}}
-				);
-			} else if (key === "progress") {
-				Notes.update(this._id,
-					{$set: {progress: value, updateAt: new Date()}}
-				);
-			} else if (key === "risk") {
-				Notes.update(this._id,
-					{$set: {risk: value, updateAt: new Date()}}
-				);
-			}
+		"click .js-note-edit": function (event) {
+			/* 进入编辑状态 */
+			var editElement = event.currentTarget;
+			var currentItem = $(".js-note-item[_id=" + this._id + "]");
+			var otherItem = $(".js-note-item[_id!=" + this._id + "]");
 
-			events.stopPropagation();
-			// return false;
-	    }
+			// 隐藏编辑按钮
+			editElement.classList.add("hide");
+			// 隐藏掉其他列表项
+			otherItem.addClass("hide");
+			// 设置当前项为全局显示
+			currentItem.addClass("absolute");
+
+			// 改变菜单栏按钮
+			$(".js-note-new").addClass("hide");
+			$(".js-note-save, .js-note-cancel, .js-note-delete").removeClass("hide");
+
+			// 转为可编辑状态
+			currentItem.find(".js-edit-title, .js-edit-content, .js-edit-progress, .js-edit-risk").attr("contenteditable", "true");
+		}
 	});
 
 }
